63bbe14ba57c0d685a5a23a613a4ae7e3fed9b22
Rafael Thiago Pereira de Melo <rafael.pereira20@icloud.com>
2021-02-12 17:20:06 -0300

    Correção de erros na tela de login

--- src/Pages/LoginPageAdmin/index.tsx
+++ src/Pages/LoginPageAdmin/index.tsx
@@ -1,23 +1,22 @@
 import React, { useCallback, useState } from 'react';
 import { Text, View, Image } from 'react-native';
 import { RectButton, ScrollView, TextInput } from 'react-native-gesture-handler';
-import { Link, useNavigation, useRoute } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import * as Yup from 'yup';
 import Logo from '../../../assets/Logo.png';
 import styles from './styles';
 import api from '../../service/api';
 
 export default function LoginPageAdmin() {
-  const [stateEmail, setStateEmail] = useState('');
-  const [statePassword, setStatePassword] = useState('');
-
   const navigation = useNavigation();
 
-  const handleLogin = useCallback(async () => {
-    try {
-      const email = stateEmail;
-      const password = statePassword;
+  const [stateEmailAdmin, setStateEmailAdmin] = useState('');
+  const [statePasswordAdmin, setStatePasswordAdmin] = useState('');
 
+  const handleLoginAdmin = useCallback(async () => {
+    try {
+      const email = stateEmailAdmin;
+      const password = statePasswordAdmin;
       const schema = Yup.object().shape({
         email: Yup.string()
           . required('Digite um e-mail válido.')
@@ -31,16 +30,18 @@ export default function LoginPageAdmin() {
 
       await schema.validate({ email, password });
 
-      const sessionUser = await api.post('session-users', {
-        email,
-        password,
+      const session = await api.post('session-admin', {
+        emailAdmin: stateEmailAdmin,
+        passwordAdmin: statePasswordAdmin,
       });
-      const { token } = sessionUser.data.createSession;
-      // const { id } = sessionUser.data.createSession.user;
-      const matchToken = await api.get(`/users-token/token/${token}`);
 
-      if (matchToken && sessionUser) {
-        navigation.navigate('SearchPage', { token });
+      const { token } = session.data;
+      const { id } = session.data.admin.id;
+
+      const matchToken = await api.get(`/admin-token/token321/${token}`);
+
+      if (matchToken && session) {
+        navigation.navigate('SearchPage', { id });
       } else {
         alert('E-mail ou senha incorreto');
       }
@@ -48,7 +49,7 @@ export default function LoginPageAdmin() {
       alert(`E-mail ou senha incorreto
             ${err}`);
     }
-  }, [stateEmail, statePassword, navigation]);
+  }, [stateEmailAdmin, statePasswordAdmin, navigation]);
 
   return (
     <View style={styles.container}>
@@ -69,22 +70,22 @@ export default function LoginPageAdmin() {
           Lembrando que essa é a área do admin, se você não é admin clique aqui
         </Text>
         <TextInput
-          value={stateEmail}
-          onChangeText={(email) => setStateEmail(email)}
+          value={stateEmailAdmin}
+          onChangeText={(email) => setStateEmailAdmin(email)}
           placeholderTextColor="#c1bccc"
           style={styles.input}
           placeholder="Digite seu email..."
           autoFocus
         />
         <TextInput
-          value={statePassword}
-          onChangeText={(password) => setStatePassword(password)}
+          value={statePasswordAdmin}
+          onChangeText={(password) => setStatePasswordAdmin(password)}
           placeholderTextColor="#c1bccc"
           style={styles.input}
           placeholder="Digite sua senha..."
           secureTextEntry
         />
-        <RectButton onPress={handleLogin} style={styles.Button}>
+        <RectButton onPress={handleLoginAdmin} style={styles.Button}>
           <View style={styles.ViewButton}>
             <Text>
               Entrar
